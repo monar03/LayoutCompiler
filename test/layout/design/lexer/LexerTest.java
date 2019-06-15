75d3e5aefbd51f269521d86959f27f61c473284b
motonari <motoyan12@gmail.com>
2019-06-15 23:16:23 +0900

    テスト追加

--- test/layout/design/lexer/LexerTest.java
+++ test/layout/design/lexer/LexerTest.java
@@ -1,6 +1,5 @@
 package layout.design.lexer;
 
-import layout.design.lexer.*;
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsInstanceOf;
 import org.junit.Test;
@@ -50,4 +49,19 @@ public class LexerTest {
         assertThat(((DesignParamResult) results.get(2)).getValue(), Is.is("1px"));
         assertThat(results.get(3), IsInstanceOf.instanceOf(DesignEndResult.class));
     }
+
+    @Test
+    public void analysis_クラスの定義_パラメータ2() {
+        final List<Result> results = new Lexer(".small-p{\n padding:1px;\n margin:1px; \n}").analysis();
+        assertThat(results.get(0), IsInstanceOf.instanceOf(ClassResult.class));
+        assertThat(results.get(1), IsInstanceOf.instanceOf(DesignStartResult.class));
+        assertThat(results.get(2), IsInstanceOf.instanceOf(DesignParamResult.class));
+        assertThat(((DesignParamResult) results.get(2)).getKey(), Is.is("padding"));
+        assertThat(((DesignParamResult) results.get(2)).getValue(), Is.is("1px"));
+        assertThat(results.get(3), IsInstanceOf.instanceOf(DesignParamResult.class));
+        assertThat(((DesignParamResult) results.get(3)).getKey(), Is.is("margin"));
+        assertThat(((DesignParamResult) results.get(3)).getValue(), Is.is("1px"));
+        assertThat(results.get(4), IsInstanceOf.instanceOf(DesignEndResult.class));
+    }
+
 }
